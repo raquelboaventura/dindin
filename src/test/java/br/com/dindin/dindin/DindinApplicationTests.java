d5b2b531b6ba385f1e800eedfc1ff27f32b20dfc
raquelboaventura <raquelb_moraes@live.com>
2025-01-22 20:32:51 -0300

    Inicializando repositório

---
+++ src/test/java/br/com/dindin/dindin/DindinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dindin.dindin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DindinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
